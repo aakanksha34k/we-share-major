0bac856c204ce1855063d1aadf5c6294e1d9b35b
Aakanksha <aakanksha.jnec@gmail.com>
2026-09-22 15:46:08 +0530

    Fix auth error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
+
+const {
+  register,
+  login,
+  googleLogin
+} = require('../controllers/authController');
+
 const router = express.Router();
-const { register, login, verifyEmail, resendVerification, googleLogin } = require('../controllers/authController');
 
 router.post('/register', register);
+
 router.post('/login', login);
-router.get('/verify-email', verifyEmail);
-router.post('/resend-verification', resendVerification);
+
 router.post('/google', googleLogin);
 
 module.exports = router;
